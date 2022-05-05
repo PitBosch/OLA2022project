1bee78dd79806571cd9f668a453403749ef0ae5f
PitBosch <62173411+PitBosch@users.noreply.github.com>
2022-05-05 14:25:56 +0200

    Update EstimatingProbabilitiesTEST.py

--- EstimatingProbabilitiesTEST.py
+++ EstimatingProbabilitiesTEST.py
@@ -9,20 +9,22 @@ def simulate_episode(init_prob_matrix, n_steps_max, alpha):
     #the 𝛼 ratios will be realizations of independent Dirichlet random variables.
     #I assume the parameter of the dirichlet to be all ones, since there's no prior assumption
     
-    elements = range(0,5)
+    elements = range(0,5) #this becomes a diagonal
     param = np.ones(6)
     alpha=np.random.dirichlet(np.ones(6),1) #I chose size=1 because we will have 6 alphas for every episode, oteherwise it's possible to generate this alpha
     #before the simulation of the episode
     #np.ones can be changed in order to give more weight to the competitor
    
-    initial_active_nodes=np.random.choice(elements, 10, p=alpha)
+    initial_active_node_idx=np.random.choice(elements, 10, p=alpha)
+    initial_active_node = np.diag(6)(initial_active_node_idx,:) #prendo l'n-esima riga della matrice diagonale, da rivedere la sintassi
 
     history= np.array([initial_active_nodes]) #from a numpy to an array
     active_nodes=initial_active_nodes
     newly_active_nodes=active_nodes
     #I think that these 3 lines can be left the same
     t=0
-    while(t<n_steps_max and np.sum(newly_active_nodes)>0): #THE SUM OF NEWLY ACTIVE NODES CAN BE LESS THAN 0?
+    #
+    while(t<n_steps_max and np.sum(newly_active_nodes)>0): #while finisce quando 
         p=(prob_matrix.T*active_nodes).T
         activated_edges=p>np.random.rand(p.shape[0], p.shape[1])
         prob_matrix=prob_matrix*((p!=0)==activated_edges)
