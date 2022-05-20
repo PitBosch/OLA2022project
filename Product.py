5524cf884fd694cb2738d38cee1b0b6c10f8455d
PietroFerretti22 <pietro2.ferretti@mail.polimi.it>
2022-05-20 13:31:54 +0200

    - Aggiunti commenti - Aggiornato il meccanismo di biforcazione tra path per renderlo simile alla versione cartacea - Collegato il margine all'utente - Inserito il sampling degli alpha e in generale la simulazione di un giorno

--- Product.py
+++ Product.py
@@ -2,7 +2,20 @@ import string
 
 
 class Product:
-    def __init__(self, price: float, label: int, name: string):
-        self.price = price
+    """ Data structure containing all the necessary informations about each product"""
+    def __init__(self, prices: [float], label: int, name: string, margins: [float]):
+        # List containing all the possible price values for the product
+        self.prices = prices
+
+        # List containing all the possible margins (margin[i] = price[i]- prod_cost) values for the product
+        self.margins = margins
+
+        # Position of the product in the product list of the environment
         self.label = label
         self.name = name
+
+    def get_daily_price(self, pos):
+        return self.prices[pos]
+
+    def get_daily_margin(self, pos):
+        return self.margins[pos]
--- User.py
+++ User.py
@@ -3,17 +3,38 @@ import pandas as pd
 
 
 class User:
+    """Class containing all the informations required to simulate the behaviour of a particular user in our website"""
     def __init__(self, alphas: np.array, res_price: float, poisson_lambda: float, probabilities: pd.DataFrame):
+        # Entry proportions between the different products (alpha_0: prob of visiting a competitor website)
         self.alphas = alphas
-        self.res_price = res_price
-        self.poisson_lambda = poisson_lambda
-        self.probabilities = probabilities
+        self.sampled_alphas = alphas
 
-    def buy(self, price):
+        # Economic availability
+        self.res_price = res_price
+
+        # Parameter of the distribution which defines the number of purchased products in case of buying
+        self.poisson_lambda = poisson_lambda
+
+        # Dataframe containing all the transition probabilities that links the different products for the user
+        self.probabilities = probabilities
+        self.margin = 0
+
+    def buy(self, price) -> bool:
+        if self.res_price > price:
+            self.update_res_price()
         return self.res_price > price
 
     def get_prod_number(self):
         return np.random.poisson(self.poisson_lambda)
 
     def start_event(self):
-        return np.random.multinomial(1, self.alphas)
+        """Method which extract the starting point of the user visit, if it returns 0 it means that the user has decided
+           to visit a competitor website. """
+        return np.random.multinomial(1, self.sampled_alphas)
+
+    def update_alphas(self):
+        self.sampled_alphas = np.random.dirichlet(self.alphas, len(self.alphas))
+
+    def update_res_price(self):
+        print(0)  # TODO: ragionare sul sistema di aggiornamento della disponibilità economica nel caso in cui l'utente effettui un acquisto.
+    # ragionevole pensare che un acquisto abbia un impatto sul budget a disposizione.
