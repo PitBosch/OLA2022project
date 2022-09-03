Ucb1 update: corrected widths formula, and changed its calculation; now t reflects the number of days, and n (that reflects the number of pulls), it's computed giving more accuration to the arms where we have more data

step3_ucb1.py
@@ -21,7 +21,7 @@ class step3_ucb1(ucb_learner):
     def update(self, arms_pulled, cr_data):
         super().update(arms_pulled, cr_data)
         for product_idx in range(self.n_products):
-            #
+            # weighted mean (on the n°_of_collected_samples so far) for the arm pulled
             mask = np.array(self.pulled)[:, 0, product_idx] == arms_pulled[product_idx]
             n_clicks = np.array(self.pulled)[:, 1, product_idx]
             n_conv = np.array(self.pulled)[:, 2, product_idx]
@@ -33,11 +33,13 @@ class step3_ucb1(ucb_learner):
             self.means[product_idx, arms_pulled[product_idx]] = product_x_arm_pulled_weighted_mean
         for product_idx in range(self.n_products):
             for arm_idx in range(self.n_arms):
-                if len(self.pulled) <= self.crs_sw:
-                    n = np.sum(np.multiply(np.array(self.pulled, dtype=np.int32)[:, 0, product_idx] == arm_idx, np.array(self.pulled, dtype=np.int32)[:, 1, product_idx]))
-                else:
-                    n = np.sum(np.multiply(np.array(self.pulled[-self.crs_sw:], dtype=np.int32)[:, 0, product_idx] == arm_idx, np.array(self.pulled[-self.crs_sw:], dtype=np.int32)[:, 1, product_idx]))
+                # n = np.sum(np.array(self.pulled, dtype=np.int32)[:, 0, product_idx] == arm_idx)
+                # n = np.sum(np.multiply(np.array(self.pulled, dtype=np.int32)[:, 0, product_idx] == arm_idx, np.array(self.pulled, dtype=np.int32)[:, 1, product_idx]))
+                n = len(self.pulled) * np.sum(
+                    np.multiply(np.array(self.pulled, dtype=np.int32)[:, 0, product_idx] == arm_idx,
+                                np.array(self.pulled, dtype=np.int32)[:, 1, product_idx])) / (
+                            np.sum(np.array(self.pulled)[:, 1]) / 20)
                 if n > 0:
-                    self.widths[product_idx, arm_idx] = np.sqrt(2 * np.log(self.t) / n)
+                    self.widths[product_idx, arm_idx] = np.sqrt(2 * np.log(self.t) / (n*(self.t - 1)))
                 else:
                     self.widths[product_idx, arm_idx] = np.inf

ucb_learner.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class ucb_learner:
     def __init__(self, n_products, n_arms, crs_sw=np.inf):
-        self.t = 0
+        self.t = 1
         self.n_products = n_products
         self.n_arms = n_arms
         self.pulled = []
@@ -12,13 +12,10 @@ class ucb_learner:
     def update(self, arms_pulled, cr_data):
         n_of_purchase_for_product = cr_data[0].astype(int)
         n_of_clicks_for_product = cr_data[1].astype(int)
-        n_of_purchase = np.sum(n_of_purchase_for_product, dtype=np.int32)
-        n_of_clicks = np.sum(n_of_clicks_for_product, dtype=np.int32)
         crs_estimation = np.divide(n_of_purchase_for_product, n_of_clicks_for_product)
         if len(self.pulled) < self.crs_sw:
             self.pulled.append([arms_pulled, n_of_clicks_for_product.tolist(), crs_estimation.tolist()])
-            self.t = np.sum(np.array(self.pulled)[:, 1])
+            self.t += 1
         else:
             self.pulled.pop(0)
             self.pulled.append([arms_pulled, n_of_clicks_for_product.tolist(), crs_estimation.tolist()])
-            self.t = np.sum(np.array(self.pulled[-self.crs_sw:])[:, 1])